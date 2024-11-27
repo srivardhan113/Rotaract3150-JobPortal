6ba525d417649a6ad30cb8e66d48fc007b20c99c
Sri Vardhan Yeluri <srivardhan.yeluri@gmail.com>
2024-11-28 01:04:31 +0530

    change

--- components/job-listing-pages/job-list/index.jsx
+++ components/job-listing-pages/job-list/index.jsx
@@ -1,5 +1,5 @@
-import FooterDefault from "/app/home/footer";
-import DefaulHeader2 from "/app/home/header";
+import FooterDefault from "/app/home/Footer";
+import DefaulHeader2 from "/app/home/Header";
 import MobileMenu from "../../header/MobileMenu";
 import FilterJobsBox from "./FilterJobsBox";
 import JobSearchForm from "./JobSearchForm";

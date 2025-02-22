428e1baea7118bd1edc68e9388b0a4f053704ab4
Sri Vardhan Yeluri <srivardhan.yeluri@gmail.com>
2025-02-22 12:26:18 +0530

    t9

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['backend.rotaracthub.in'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'backend.rotaracthub.in',
-          pathname: '/api/companies/get-image/**',
-        },
-      ],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'backend.rotaracthub.in',
+        pathname: '/api/companies/get-image/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
